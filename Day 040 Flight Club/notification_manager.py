a014e8147df335f47a332985a765834f0f64740c
cr-carter <165337768+cr-carter@users.noreply.github.com>
2024-12-26 14:34:31 -0600

    Create notification_manager.py

---
+++ Day 040 Flight Club/notification_manager.py
@@ -0,0 +1,62 @@
+import os
+from twilio.rest import Client
+import smtplib
+
+class NotificationManager:
+
+    def __init__(self):
+        self.client = Client(os.environ['ACCOUNT_SID'], os.environ["AUTH_TOKEN"])
+        self.email = os.environ.get('EMAIL_ADDRESS')
+        self.email_password = os.environ.get('EMAIL_PASSWORD')
+        self.connection = smtplib.SMTP(os.environ["SMTP_ADDRESS"])
+
+    def send_sms(self, message_body):
+        """
+        Sends an SMS message through the Twilio API.
+        This function takes a message body as input and uses the Twilio API to send an SMS from
+        a predefined virtual number (provided by Twilio) to your own "verified" number.
+        It logs the unique SID (Session ID) of the message, which can be used to
+        verify that the message was sent successfully.
+
+        Parameters:
+        message_body (str): The text content of the SMS message to be sent.
+
+        Returns:
+        None
+
+        Notes:
+        - Ensure that `TWILIO_VIRTUAL_NUMBER` and `TWILIO_VERIFIED_NUMBER` are correctly set up in
+        your environment (.env file) and correspond with numbers registered and verified in your
+        Twilio account.
+        - The Twilio client (`self.client`) should be initialized and authenticated with your
+        Twilio account credentials prior to using this function when the Notification Manager gets
+        initialized.
+        """
+        message = self.client.messages.create(
+            from_=os.environ["TWILIO_NUM"],
+            body=message_body,
+            to=os.environ["ALERT_NUM"]
+        )
+        # Prints if successfully sent.
+        print(message.sid)
+
+    # Is SMS not working for you or prefer whatsapp? Connect to the WhatsApp Sandbox!
+    # https://console.twilio.com/us1/develop/sms/try-it-out/whatsapp-learn
+    def send_whatsapp(self, message_body):
+        message = self.client.messages.create(
+            from_=f'whatsapp:{os.environ["TWILIO_NUM"]}',
+            body=message_body,
+            to=f'whatsapp:{os.environ["ALERT_NUM"]}'
+        )
+        print(message.sid)
+
+    def send_emails(self, email_list, email_body):
+        with self.connection:
+            self.connection.starttls()
+            self.connection.login(self.email, self.email_password)
+            for email in email_list:
+                self.connection.sendmail(
+                    from_addr=self.email,
+                    to_addrs=email,
+                    msg=f'Subject:New Flight Found\n\n{email_body}'
+                )
